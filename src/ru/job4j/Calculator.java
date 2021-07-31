f52a1f2580844a2c898e8114856a849930c453dc
valera <vzavalko@gmail.com>
2021-07-31 08:40:42 +0100

    3.2. Фиксация изменений на github [#243564]

--- src/ru/job4j/Calculator.java
+++ src/ru/job4j/Calculator.java
@@ -5,9 +5,13 @@ public class Calculator {
         int six = 6;
         int four = 4;
         int five = 5;
-        int sixDivTwo = six/2;
-        int fiveMinusTwo = five - 2;
-        int fourTimesTwo = four * 2;
+        int one = 1;
+        int two = 2;
+        int onePlusTwo = one + two;
+        System.out.println(onePlusTwo);
+        int sixDivTwo = six/two;
+        int fiveMinusTwo = five - two;
+        int fourTimesTwo = four * two;
         System.out.println(sixDivTwo);
         System.out.println(fourTimesTwo);
         System.out.println(fiveMinusTwo);
