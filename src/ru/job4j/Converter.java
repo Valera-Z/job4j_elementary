9b210439234bc47076ea04114a97a6471a7c385a
valera <vzavalko@gmail.com>
2021-08-02 21:04:19 +0100

    3.2. Фиксация изменений на github [#243564]

--- src/ru/job4j/Converter.java
+++ src/ru/job4j/Converter.java
@@ -2,19 +2,26 @@ package ru.job4j;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        float rsl = value / 70;
-        return rsl;
+        return value / 70;
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value/60; /* формула перевода рублей в доллары. */
-        return rsl;
+        return value / 60;
     }
 
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2 euros. Test result : " + passed);
         float dollar = Converter.rubleToDollar(100);
         System.out.println("100 rubles are " + dollar + " dollars.");
+        in = 120;
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println("120 rubles are 2 dollars. Test result : " + passed);
     }
 }
