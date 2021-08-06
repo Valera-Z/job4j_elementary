985da39a8ee27a07ed5c2e11e81ba822d2d532b5
valera <vzavalko@gmail.com>
2021-08-06 13:34:42 +0100

    Changes to "math" 1.

---
+++ src/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double differenceAndDivision(double first, double second) {
+        return difference(first, second)
+                + divide(first, second);
+    }
+
+    public static double totalOfMethods(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second)
+                    + difference(first, second)
+                        +divide(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + differenceAndDivision(10, 20));
+        System.out.println("Результат расчета равен: " + totalOfMethods(10, 20));
+    }
+}
